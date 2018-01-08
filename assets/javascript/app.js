e7305caaf3680e330bc9fefb806f7823cb3a2440
e30devo <dthomase30@gmail.com>
2018-01-07 16:54:21 -0800

    Major CSS and JS push
    Fixed repeating gif issue, now showing 10 per request
    as noted in hw instructions.
    
    Start/Stop on.click functionality present as of this commit.
    
    Removed console.log notation.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
-var queryPath = "https://api.giphy.com/v1/gifs/search?"
-var apiKey = "api_key=pG74Ez5xu2bshU6HguRdNRcvi8FZUh1E"
-var topics = ["Taylor Swift", "Bill Murray",]
-
+var queryPath = "https://api.giphy.com/v1/gifs/search?";
+var apiKey = "api_key=pG74Ez5xu2bshU6HguRdNRcvi8FZUh1E";
+var topics = ["Taylor Swift", "Bill Murray", "Aaron Rodgers", "Jennifer Aniston", "Brad Pitt"];
+var limitReq = "&limit=10";
+var rating = "&rating=g"
 
 function renderButtons() {
     for (var i = 0  ; i < topics.length; i++) {
-        var topicButton = $("<button>");
-        console.log(topicButton);
+        var topicButton = $("<button>");        
         topicButton.addClass("button");
         topicButton.attr("data", topics[i]);
         topicButton.text(topics[i]);
@@ -19,36 +19,44 @@ function renderButtons() {
 renderButtons();
 
 $("button").on("click", function (){
-    var searchTopic = "&q=" + $(this).attr("data").replace(/\s+/, "");
+    $(".main-container").empty();
+    var searchTopic = "&q=" + $(this).attr("data").replace(/\s/, "+");
     console.log(searchTopic);
     $.ajax({
-        url: queryPath + apiKey + searchTopic,
+        url: queryPath + apiKey + searchTopic + limitReq + rating,
         method: "GET",
     })
 
     .done(function(response) {
         console.log(response);
         console.log(response.data.length);
-
         for (var i = 0; i < response.data.length; i++)   {     
-        var smallGIF = response.data[[i]].images.original.url;
+        var smallGIF = response.data[i].images.original.url;
+        var staticGIF = response.data[i].images.original_still.url;
         console.log(smallGIF);
-        $(".main-container").append("<img class=results>");        
-        $(".results").attr("src", smallGIF);
-        }
+        var gifContainer = $("<div>");
+        var gifImage = $("<img>");
+        var ratingContainer = $("<div>");
+        gifContainer.addClass("sub-container");        
+        gifImage.addClass("results");
+        gifImage.attr("data", smallGIF);
+        gifImage.attr("src", staticGIF);
+        ratingContainer.addClass("ratingContainer")
+        ratingContainer.text("Rating: " + response.data[i].rating)            
+        $(gifContainer).append(gifImage);
+        $(gifContainer).append(ratingContainer); 
+        $(".main-container").append(gifContainer);
+        } //for loop closer
+        
+        $("img").on("click", function() {
+            var src = $(this).attr("src");
+            var data = $(this).attr("data");
+            $(this).attr("data", data.replace(data, src));
+            $(this).attr("src", src.replace(src, data));
 
-// <!-- These are hopefully created dynamically in app.js
-//     <div class="results"></div>
-// <div class="gif"></div>
-// <div class="rating"></div> -->
+        }); //img on.click closer       
     }); //.done closer
-}); //on.click closer
-
-
-
-//  onclick "this" ,query path 
-// for loop for div containing div for gif and div for rating, patterned nicely
-
+}); //button on.click closer
 
 
 }); //close for document.ready
